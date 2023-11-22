61168dcb241541d5f5768915cc0244c2e722748f
wfscarvalho <wfs.carvalho@gmail.com>
2023-11-22 20:31:01 -0300

    v1 - xp do heroi

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
-let nome = "Wesley"
+let nomeDoHeroi="João"
+let expDoHeroi=19999
+let RankDoHeroi
 
-console.log("oi "+nome)
+//console.log("Informe qual é o nome do Heroi: ")
+
+//função para armazenar valor em javascript
+
+//console.log("informe qual a Exp do Heroi: ")
+
+//função para armazenar valor
+
+if (expDoHeroi<1000){
+    RankDoHeroi="Ferro"}
+
+else if ((expDoHeroi>=1000) && (expDoHeroi<=2000)){
+    RankDoHeroi="Bronze"}
+
+else if ((expDoHeroi>=2000) && (expDoHeroi<=3000)){
+    RankDoHeroi="Prata"}
+
+else if ((expDoHeroi>=3000) && (expDoHeroi<=4000)){
+    RankDoHeroi="Ouro"}
+
+else if ((expDoHeroi>=4000) && (expDoHeroi<=5000)){
+    RankDoHeroi="Platina"}
+
+else if ((expDoHeroi>=5000) && (expDoHeroi<=6000)){
+    RankDoHeroi="Ascendente"}
+
+else if ((expDoHeroi>=6000) && (expDoHeroi<=7000)){
+    RankDoHeroi="Imortal"}
+
+else if ((expDoHeroi>=7000) && (expDoHeroi<=8000)){
+    RankDoHeroi="Radiante"}
+
+else if ((expDoHeroi>=8000) && (expDoHeroi<=9000)){
+    RankDoHeroi="Semi-Deus"}
+
+else if ((expDoHeroi>=9000) && (expDoHeroi<=10000)){
+    RankDoHeroi="Deus"}
+
+else if ((expDoHeroi>10000)){
+    RankDoHeroi="Deus Supremo"}
+
+
+
+
+console.log("O Herói "+nomeDoHeroi+" Está com "+expDoHeroi+" de Exp. Então ele é nível "+RankDoHeroi)
